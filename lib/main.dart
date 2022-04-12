a44d612120c40ced36efa1c9336045434dcc67ce
glofiego10 <glofiego10@gmail.com>
2022-04-12 13:14:24 -0300

    AppBar elevation on scroll, cambios menores y coneccion de Firebase Mejorada

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:appbar_elevation/appbar_elevation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
 import 'package:flutter_website/ui/block_wrapper.dart';
 import 'package:flutter_website/ui/carousel/carousel.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'components/components.dart';
 import 'ui/blocks.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBW7QsrfGVWJ_BTImiFkmmDMDJvfoHT0DQ",
+          authDomain: "soludev-web.firebaseapp.com",
+          projectId: "soludev-web",
+          storageBucket: "soludev-web.appspot.com",
+          messagingSenderId: "73781584016",
+          appId: "1:73781584016:web:2fdc7e6b5c6390fd353815"));
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +44,31 @@ class MyApp extends StatelessWidget {
             const ResponsiveBreakpoint.resize(1080, name: DESKTOP),
           ],
           background: Container(color: background)),
-      home: Scaffold(
-        appBar: const PreferredSize(
-            preferredSize: Size(double.infinity, 66), child: MenuBar()),
-        body: ListView.builder(
-            itemCount: blocks.length,
-            itemBuilder: (context, index) {
-              return blocks[index];
-            }),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            // ignore: avoid_print
+            print("error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ScrollActivatedAppBarElevation(
+                builder: (BuildContext context, double appBarElevation) {
+              return Scaffold(
+                appBar: PreferredSize(
+                    preferredSize: const Size(double.infinity, 66),
+                    child: Material(
+                        elevation: appBarElevation, child: const MenuBar())),
+                body: ListView.builder(
+                    itemCount: blocks.length,
+                    itemBuilder: (context, index) {
+                      return blocks[index];
+                    }),
+              );
+            });
+          }
+          return const CircularProgressIndicator();
+        },
       ),
       debugShowCheckedModeBanner: false,
     );
@@ -50,22 +82,9 @@ List<Widget> blocks = [
       defaultScale: true,
       mediaQueryData: const MediaQueryData(size: Size(1200, 640)),
       child: RepaintBoundary(child: Carousel())),
-  const BlockWrapper(GetStarted()),
   const BlockWrapper(Features()),
   const BlockWrapper(FastDevelopment()),
   const BlockWrapper(BeautifulUI()),
-  const BlockWrapper(NativePerformance()),
-  const BlockWrapper(LearnFromDevelopers()),
-  const BlockWrapper(WhoUsesFlutter()),
-  const BlockWrapper(FlutterNewsRow()),
   const BlockWrapper(InstallFlutter()),
   const Footer(),
 ];
-
-// Disabled codelab block for performance.
-//              ResponsiveVisibility(
-//                hiddenWhen: [Condition.smallerThan(name: DESKTOP)],
-//                child: ResponsiveConstraints(
-//                    constraintsWhen: blockWidthConstraints,
-//                    child: FlutterCodelab()),
-//              ),
